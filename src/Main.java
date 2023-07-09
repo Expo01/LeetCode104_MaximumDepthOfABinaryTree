51f8c7ccf3afab9090d3aa5a10062024366ae749
Expo <crdailey01@gmail.com>
2023-07-08 22:32:26 -0700

    review math.max leetcode problem

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.tree.TreeNode;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+
+    }
+}
